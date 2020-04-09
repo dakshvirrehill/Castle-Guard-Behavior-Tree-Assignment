492c2d930679842437b5ae65116b62c8a74f510b
Dakshvir Rehill <dakshvirr@gmail.com>
2020-04-09 13:22:28 -0400

    Added guard script which is a container for the guard values

---
+++ Assets/Game/Scripts/CastleGuard/CastleGuard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CastleGuard : MonoBehaviour
+{
+    public OffMeshLink mBridgeLink;
+    public NavMeshAgent mNavMeshAgent;
+    public Animator mAnimator;
+    [SerializeField] float mHealth = 100;
+    public float mDamageAmount = 10;
+    public List<Transform> mPatrolWaypoints;
+    [HideInInspector]public List<Vector3> mInvestigationPoints;
+    public float GuardHealth 
+    { 
+        get
+        {
+            return mHealth;
+        }
+        set
+        {
+            mHealth = value;
+        }
+    }
+
+    public int InvestigationPointCount
+    {
+        get
+        {
+            return mInvestigationPoints.Count;
+        }
+        set
+        {
+
+        }
+    }
+
+}
